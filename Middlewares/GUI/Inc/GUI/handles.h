7eb428ba104d9ad19be95092c3d97b2a862d9e2b
Shiroko <hhx.xxm@gmail.com>
2021-12-12 22:44:31 +0800

    aa

--- Middlewares/GUI/Inc/GUI/handles.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GUI_HANDLES_H__
-#define __GUI_HANDLES_H__
-
-void switch_to_inuse_scr(const char *user);
-void switch_to_login_scr();
-void switch_to_loading_scr();
-
-#endif // __GUI_HANDLES_H__
--- Middlewares/GUI/Inc/GUI/lvgl.h
+++
@@ -1,3 +0,0 @@
-#include "lvgl/lvgl.h"
-
-#define LV_CAST_TO_OBJ(x) ((lv_obj_t *)(x))
